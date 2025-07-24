d7179828384ca94813f40df9fbf8c0fb9d090a7f
Tim <Timpo88m@gmail.com>
2025-07-24 03:04:58 -0500

    Update Capability.swift
    <3 GIT+XCODE

--- Giskard/Giskard/Code/Capabilities/Capability.swift
+++ Giskard/Giskard/Code/Capabilities/Capability.swift
@@ -5,7 +5,29 @@
 //  Created by Timothy Powell on 7/24/25.
 //
 
-struct Capability: Codable {
-    let name: String = "Base Capability"
-    var enabled: Bool = true
+import Foundation
+
+class Capability: Codable {
+    
+    init(_ entityID: UUID, enabled: Bool = true) {
+        self.entity = entityID
+        self.entityInstance = EntitySystem.getEntityById(entityID)
+        self.enabled = enabled
+    }
+    
+    init(_ entity: Entity, enabled: Bool = true) {
+        self.entityInstance = entity
+        self.entity = entity.id
+        self.enabled = enabled
+    }
+    
+    public var enabled: Bool = true
+    public var entity: UUID
+    
+    private var entityInstance: Entity?
+    
+    enum CodingKeys: String, CodingKey {
+        case enabled
+        case entity
+    }
 }
